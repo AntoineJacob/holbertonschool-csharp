841b3d9947996b56e2321dd0aecc1719d601b093
AntoineJacob <5646@holbertonstudents.com>
2023-12-05 15:39:18 +0100

    task 3

---
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -0,0 +1,8 @@
+﻿﻿using System;
+    class writeline 
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("\"Programming is like building a multilingual puzzle");
+        }  
+    }
