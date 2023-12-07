3af150485fbee792d363ce0158962c2f1965f985
AntoineJacob <5646@holbertonstudents.com>
2023-12-07 14:18:38 +0100

    task 5

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,25 @@
+﻿﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            for (i = 0; i <= 99; i++)
+            {
+                if (i < 99)
+                {
+                   Console.Write($"{i:D2}, ");
+                }
+                else
+                {
+                    Console.WriteLine($"{i}");
+                }
+
+
+            }
+        }
+    }
+}
