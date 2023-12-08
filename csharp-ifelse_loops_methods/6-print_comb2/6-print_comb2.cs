bb50c1cf4dc98db29158e126f49b84b673ed3531
AntoineJacob <5646@holbertonstudents.com>
2023-12-08 12:46:11 +0100

    task 6

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,30 @@
+﻿﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int k = 1;
+
+            for (i = 0; i <= 9; i++)
+            {
+                for (int j = k; j <= 9; j++)
+                {
+                    if (i < 8)
+                    {
+                        Console.Write(i);
+                        Console.Write($"{j}, ");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{i}{j}");
+                    }
+                }
+                k++;
+            }
+        }
+    }
+}
