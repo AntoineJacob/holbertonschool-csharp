2b74f49a13b08c183fa57bec7344ac314d990c37
AntoineJacob <5646@holbertonstudents.com>
2023-12-11 12:33:06 +0100

    task 1

---
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+public static class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+        int arrayLenght = array.Length;
+        if (index >= 0 && index < arrayLenght)
+         {
+            return (array[index]);
+        }
+        else
+        {
+            Console.WriteLine("Index out of range");
+            return (-1);
+        }
+    }
+}
+
