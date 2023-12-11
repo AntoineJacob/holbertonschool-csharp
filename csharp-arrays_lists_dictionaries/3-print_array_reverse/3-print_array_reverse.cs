cc7c56f0f1ae5e4f3fab862d1628476c6ac34e51
AntoineJacob <5646@holbertonstudents.com>
2023-12-11 13:29:29 +0100

    task 3

---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,27 @@
+﻿﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            Console.WriteLine("");
+        }
+        else
+        {
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine($"{array[i]}");
+                }
+                else
+                {
+                    Console.Write($"{array[i]} ");
+                }
+
+            }
+        }
+    }
+}
