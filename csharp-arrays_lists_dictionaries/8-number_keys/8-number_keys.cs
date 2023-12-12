d9d3b81651e939f70b64cb6eac80cc1ad5fea7dd
AntoineJacob <5646@holbertonstudents.com>
2023-12-12 11:39:56 +0100

    task 8

---
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Dictionary
+    {
+        public static int NumberOfKeys(Dictionary<string, string> myDict)
+        {
+            int NumberOfKeys = 0;
+            foreach (var key in myDict)
+            {
+                NumberOfKeys++;
+            }
+            return NumberOfKeys;
+        }
+    }
