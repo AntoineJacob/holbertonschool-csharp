ac2c98975adceec0a1ba0d7270d565c1186411f0
AntoineJacob <5646@holbertonstudents.com>
2023-12-14 12:03:18 +0100

    task 5

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> myLList = new LinkedList<int>();
+
+        for (int i = 0; i < size; i++)
+        {
+            myLList.AddLast(i);
+        }
+        foreach (int item in myLList)
+        {
+            Console.WriteLine(item);
+        }
+        return myLList;
+    }
+}
