49d16dc3bf4c1a2c4cb52202dd230f345d5f33a8
AntoineJacob <5646@holbertonstudents.com>
2024-01-08 12:51:45 +0100

    task 0 and Readme

---
+++ csharp-classes/0-enemy/0-enemy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Enemies
+{
+    class Zombie
+    {
+        static void Enemies(string[] args)
+        {
+            Console.WriteLine("Empty class");
+        }
+    }
+}
