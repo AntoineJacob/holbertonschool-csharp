2b94f8d8d93670558947b5e154af73529595b6b3
AntoineJacob <5646@holbertonstudents.com>
2024-01-08 12:57:54 +0100

    task 1

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>
+    ///public class Zombie
+    ///</summary>
+    public class Zombie
+    {
+        ///<summary>
+        ///public int health
+        ///</summary>
+        public int health = 0;
+    }
+}
