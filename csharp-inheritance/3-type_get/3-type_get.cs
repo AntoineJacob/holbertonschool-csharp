535e3672ed2c0d84eb02d0a3e60534fbb0085dab
AntoineJacob <5646@holbertonstudents.com>
2024-01-15 14:05:23 +0100

    pls work task 3

--- csharp-inheritance/3-type_get/3-type_get.cs
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -11,22 +11,17 @@ class Obj
     /// method that prints the names of the
     /// available properties and methods of an object.
     /// </summary>
-    public static void Print(object myObj)
+     public static void Print(object myObj)
     {
         var type = myObj.GetType();
-        Console.WriteLine(type.Name + " Propeties:");
+        Console.WriteLine(type.Name + " Properties:");
 
-        foreach (PropertyInfo property in  type.GetProperties())
-        {
+        foreach (PropertyInfo property in type.GetProperties())
             Console.WriteLine(property.Name);
-        }
-
+            
         Console.WriteLine(type.Name + " Methods:");
 
         foreach (MethodInfo method in type.GetMethods())
-        {
             Console.WriteLine(method.Name);
-        }
     }
 }
-
