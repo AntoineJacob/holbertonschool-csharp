8802b4820fcb0a25ee0763ad3468f1f85adf4567
AntoineJacob <5646@holbertonstudents.com>
2024-01-15 13:43:29 +0100

    task 2

---
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Class Obj
+/// </summary>
+class Obj
+{
+    /// <summary>
+    ///  method that returns True if the object is an instance of a class that inherits from the specified class,
+    ///  otherwise return False.
+    ///  The object must be a subclass
+    ///  ; your method cannot return True if the object is an instance of the base class.
+    /// </summary>
+
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.IsSubclassOf(baseType))
+        {
+            if (derivedType != baseType)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
