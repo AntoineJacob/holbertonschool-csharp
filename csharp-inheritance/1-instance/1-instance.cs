34f4a99b38f0cbe9ec1300ff37c0d654d7d98c5c
AntoineJacob <5646@holbertonstudents.com>
2024-01-15 13:26:54 +0100

    task 1

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Class Obj
+/// </summary>
+class Obj
+{
+    /// <summary>
+    ///  method that returns True if the object is an instance of, 
+    ///  or if the object is an instance of a class that inherited from, Array, otherwise return False
+    /// </summary>
+
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return typeof(Array).IsAssignableFrom(obj.GetType());
+    }
+}
