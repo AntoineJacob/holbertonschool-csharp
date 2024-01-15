dd550b704a2af672f7de73cbbc607a44588b2210
AntoineJacob <5646@holbertonstudents.com>
2024-01-15 14:24:58 +0100

    task 5

---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// Class Shape
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// public virtual int Area()
+    /// </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
