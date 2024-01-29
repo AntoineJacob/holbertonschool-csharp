31e090fe7cf1de121474e1d3fc7d5b18dbde1634
AntoineJacob <5646@holbertonstudents.com>
2024-01-29 14:07:22 +0100

    task 16

---
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+/// <summary>
+/// Class MatrixMath
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// Method that multiplies a matrix and a scalar and returns the resulting matrix.
+    /// </summary>
+    /// <param name="matrix"></param>
+    /// <param name="scalar"></param>
+    /// <returns></returns>
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        int rows = matrix.GetLength(0);
+        int columns = matrix.GetLength(1);
+
+        // Check if the matrix is either 2D or 3D
+        if ((rows != 2 && rows != 3) || rows != columns)
+        {
+            // Return a matrix containing -1 for invalid matrix
+            return new double[,] { { -1 } };
+        }
+
+        double[,] result = new double[rows, columns];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                result[i, j] = matrix[i, j] * scalar;
+            }
+        }
+
+        return result;
+    }
+
+    public static void PrintMatrix(double[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int columns = matrix.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{matrix[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
