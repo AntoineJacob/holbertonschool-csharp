920d028f5e36435a87ded63ef489f316ad4005a8
AntoineJacob <5646@holbertonstudents.com>
2024-01-29 11:41:33 +0100

    task 3

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+/// <summary>
+/// Class VectorMath
+/// </summary>
+class VectorMath
+{
+    /// <summary>
+    ///  Method that calculates and returns the length of a given vector.
+    /// </summary>
+    /// <param name="vector">It contain the pos</param>
+    /// <returns></returns>
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length != 2)
+        {
+            throw new ArgumentException("Vector must be 2-dimensional.");
+        }
+
+        double x = vector[0];
+        double y = vector[1];
+
+        // Pythagorean theorem: length = sqrt(x^2 + y^2)
+        double length = Math.Sqrt(x * x + y * y);
+
+        return length;
+    }
+}
