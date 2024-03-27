683346346c3a08d113f25836418987d6e63e5d09
AntoineJacob <5646@holbertonstudents.com>
2024-03-27 11:10:25 +0100

    first commit with readme and task 0

---
+++ csharp-generics/0-queue/queue.cs
@@ -0,0 +1,12 @@
+﻿﻿using System;
+/// <summary>
+/// New generic list Queue of type T
+/// </summary>
+/// <typeparam name="T"></typeparam>
+class Queue<T>
+{
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}
